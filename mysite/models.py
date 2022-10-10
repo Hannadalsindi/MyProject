bf4c1b75cea3253ff902bcfa6e89c016721f5c7e
hannadalsindi <hannadalsindi@gmail.com>
2022-10-10 14:44:43 +0300

    init

--- mysite/models.py
+++ mysite/models.py
@@ -8,16 +8,25 @@ from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
+    
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone_num = models.CharField(max_length=30, blank=True)
     location = models.CharField(max_length=30, blank=True)
+    dept = models.CharField(max_length=30, blank=True)
     birth_date = models.DateField(blank=True, null=True)
-    
+    GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+    )
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, default="user")
+
     
 
     def __str__(self):
         return self.user.username
    
+
+
 
 
 
