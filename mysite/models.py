761294d7a062e77b52afdcc468827934fe745f89
hannadalsindi <hannadalsindi@gmail.com>
2022-10-10 11:15:46 +0300

    remarks

---
+++ mysite/models.py
@@ -0,0 +1,28 @@
+
+from random import choices
+from unittest.util import _MAX_LENGTH
+from django.db import models
+from django.contrib.auth.models import User 
+
+
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone_num = models.CharField(max_length=30, blank=True)
+    location = models.CharField(max_length=30, blank=True)
+    birth_date = models.DateField(blank=True, null=True)
+    
+    
+
+    def __str__(self):
+        return self.user.username
+   
+
+
+
+
+   
+
+
+
